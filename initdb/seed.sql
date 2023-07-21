c29621905134f1c7da23cab901400559d68833ec
azwan11 <azwanalkhawarismi@gmail.com>
2023-07-21 20:03:50 +0700

    delete

--- initdb/seed.sql
+++ initdb/seed.sql
@@ -1,9 +1,9 @@
 -- sql yang akan dijalankan saat container appDb started,
 -- contoh:
 
-CREATE DATABASE IF NOT EXISTS contoh;
+CREATE DATABASE IF NOT EXISTS News;
 
-CREATE TABLE IF NOT EXISTS contoh.orang(
+CREATE TABLE IF NOT EXISTS news.users(
     nama varchar(20),
     alamat varchar(20)
 );
